3917dcf2d88379d2c669c56fde0647f298c676d4
RazielBlaster <jhojanblackout@gmail.com>
2023-04-17 12:41:42 -0500

    Start pjetc and cards

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import CardProducts from "./assets/components/CardProducts";
+
+function App() {
+
+
+
+  return <div className="App">
+
+    <CardProducts/>
+
+  </div>;
+}
+
+export default App;
---
+++ src/assets/components/CardProducts.jsx
@@ -0,0 +1,27 @@
+import products from "../json/products.json";
+
+const CardProducts = () => {
+
+    return (
+        <section className="container-products">
+            {
+                products.map(product => (
+                    <div className="product_element" key={product.id}>
+                        <div className="product-element-container-img">
+                            <img className="product-element-img" src={product.image} alt={product.description} />
+                        </div>
+                        <div className="product-element-main">
+                            <h3 className="product-name" >{product.name}</h3>
+                            <p className="product-price" >{product.price.toLocaleString('es-CO', { style: 'currency', currency: 'COP', minimumFractionDigits: 0 })}</p>
+                            <p className="product-measures">{product.measures}</p>
+                        </div>
+
+                    </div>
+                ))
+            }
+        </section>
+    )
+}
+
+
+export default CardProducts
