a083df600c786318f34fa1c8d33ee4c81ca9495b
RazielBlaster <jhojanblackout@gmail.com>
2023-04-18 18:20:23 -0500

    Add map to pokets

--- src/assets/components/CardProducts.jsx
+++ src/assets/components/CardProducts.jsx
@@ -1,38 +1,41 @@
 import { useRef } from "react";
 import products from "../json/products.json";
 
-
 const CardProducts = () => {
+    const descriptions = useRef([]);
 
-    
-    const descriptions = useRef([])
-    
-
-    const handleClick = (id) =>{        
-        descriptions.current[id].classList.toggle('product-description-show')        
+    const handleClick = (id) => {
+        descriptions.current[id].classList.toggle('product-description-show');
     }
 
     return (
         <section className="container-products">
             {
                 products.map((product, id) => (
-                    <div key={product.id} className="product_element" >
+                    <div key={product.id} className="product_element">
                         <div onClick={() => handleClick(id)} className="product-element-container-img">
                             <img className="product-element-img" src={product.image} alt={product.description} />
                         </div>
 
                         <div className="product-element-main">
-                            <h3 className="product-name" >{product.name}</h3>
-                            <p className="product-price" >{product.price.toLocaleString('es-CO', { style: 'currency', currency: 'COP', minimumFractionDigits: 0 })}</p>
+                            <h3 className="product-name">{product.name}</h3>
+                            <p className="product-price">{product.price.toLocaleString('es-CO', { style: 'currency', currency: 'COP', minimumFractionDigits: 0 })}</p>
                             <p className="product-measures">{product.measures}</p>
                         </div>
 
                         <div onClick={() => handleClick(id)} className="product-container-description" ref={element => descriptions.current[id] = element}>
                             <p>{product.description}</p>
-                            <br />
+
+                            {
+                                product.pockets && product.pockets.map((pocket, index) => (
+                                    <ul key={index}>
+                                        <li> - {pocket}</li>
+                                    </ul>
+                                ))
+                            }
+
                             <br />
                         </div>
-
                     </div>
                 ))
             }
@@ -40,5 +43,4 @@ const CardProducts = () => {
     )
 }
 
-
-export default CardProducts
+export default CardProducts;
