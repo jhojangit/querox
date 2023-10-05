ac7dbf35317008cd2ac99fbb8487c86fc25c6054
RazielBlaster <jhojanblackout@gmail.com>
2023-10-05 10:04:22 -0500

    Add url img product in btn whatsapp

--- src/assets/components/modal/Modal.jsx
+++ src/assets/components/modal/Modal.jsx
@@ -7,10 +7,11 @@ import logo from "../../../../public/img-products/LOGO1.png"
 
 
 const Modal = ({ product, onProductExit }) => {
+    let urr = "https://res.cloudinary.com/dojw0dkya/image/upload/v1696467681/ecommerce-api/mouse-rgb3.png"
 
-    
 
-    const nameProductURL = `https://wa.me/573223250286?text=Hola%20Me%20gustaría%20comprar%20este%20producto%20${product.name}$20=%20${product.price}`
+
+    const nameProductURL = `https://wa.me/573223250286?text=Hola%20Me%20gustaría%20comprar%20este%20producto%20${product.name}%20=%20${product.price}%20${product.imageUrl}`
 
     return (
         <div className='modal__product'>
